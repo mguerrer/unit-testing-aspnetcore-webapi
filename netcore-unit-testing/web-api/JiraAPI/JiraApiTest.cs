1dc2943379aaa7488ed449c973f23104b1b485c9
Marcos Guerrero <marcosguerrerow@hotmail.com>
2022-03-23 12:11:19 -0300

    Adds Jira API test with RestSharp.

---
+++ netcore-unit-testing/web-api/JiraAPI/JiraApiTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using RestSharp;
+using Xunit;
+
+namespace jira_tests
+{
+    public class JiraApiTest
+    {
+
+        [Fact]
+        public void Issue_Add_Comment()
+        {
+            var client = new RestClient("https://marcosguerrerow.atlassian.net/rest/api/2/issue/PRO-10/comment");
+            client.Timeout = -1;
+            var request = new RestRequest(Method.POST);
+            request.AddHeader("Content-Type", "application/json");
+            request.AddHeader("Authorization", "Basic bWFyY29zZ3VlcnJlcm93QGhvdG1haWwuY29tOlZpMHJYUjJlVW9wcldJd0tZU3JwMTczMA==");
+            var body = @"{""body"":""Comentario desde Azure Devops""}";
+            request.AddParameter("application/json", body, ParameterType.RequestBody);
+            IRestResponse response = client.Execute(request);
+            Console.WriteLine(response.Content);
+        }
+    }
+}
+
