c28f7f9d2e597a7efa37c18a53e4a835c5a3fe0e
Marcos Guerrero <marcosguerrerow@hotmail.com>
2022-03-23 15:20:20 -0300

    Adding notes and cosmetic changes.

--- JiraAPI/JiraApiTest.cs
+++ JiraAPI/JiraApiTest.cs
@@ -15,10 +15,13 @@ namespace jira_tests
             var request = new RestRequest(Method.POST);
             request.AddHeader("Content-Type", "application/json");
             request.AddHeader("Authorization", "Basic bWFyY29zZ3VlcnJlcm93QGhvdG1haWwuY29tOlZpMHJYUjJlVW9wcldJd0tZU3JwMTczMA==");
-            var body = @"{""body"":""Comentario desde Azure Devops""}";
+            var body = @"{""body"":""Comment from GitHub pipeline...""}";
             request.AddParameter("application/json", body, ParameterType.RequestBody);
             IRestResponse response = client.Execute(request);
             Console.WriteLine(response.Content);
+
+            // Assert
+            Console.WriteLine("Status code= " + response.StatusCode);
         }
     }
 }
